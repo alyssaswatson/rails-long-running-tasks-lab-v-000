2816d7bc9bf6af5420e72c5681c3510a6e43aadd
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-09-10 18:10:40 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
-    redirect_to customers_path
+    redirect_to songs_path
   end
 
   def edit
